4651530ff34e7de7fc280da1c2d02fb76cb5cca3
LeonardoPell <leonardojbpellegrino@gmail.com>
2024-01-26 09:22:49 -0300

    chore: adicionando mensagens exception

--- src/core/exceptions/exception.message.ts
+++ src/core/exceptions/exception.message.ts
@@ -1,3 +1,11 @@
 export function notFoundExceptionMessage(item: string){
     return `${item} was not found`;
 }
+
+export function notAcceptableTypeParamMessage(item: string, type: string){
+    return `${item} are not a ${type}`;
+}
+
+export function badRequestExceptionParamMessage(item: string, type: string){
+    return `${item} are not a ${type}`;
+}
