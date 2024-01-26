3bb6a1f9ab0bd1ca81c3d60f3c785f70465ae120
LeonardoPell <leonardojbpellegrino@gmail.com>
2024-01-26 09:21:24 -0300

    feat: alterando validacao direta na funcao

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,21 +1,22 @@
-import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotAcceptableException, NotFoundException } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { notFoundExceptionMessage } from 'src/core/exceptions/exception.message';
-import isNumberOrThrow from 'src/core/functions/validators/isNumberOrThrow';
+import { notAcceptableTypeParamMessage, notFoundExceptionMessage } from 'src/core/exceptions/exception.message';
 import { PrismaService } from 'src/prisma-module/prisma.service';
 
 
 @Injectable()
 export class UserService {
-
+  
   constructor(private _prismaService: PrismaService) {}
 
   async findOneById(userId: number): Promise<User> {
 
-    userId = isNumberOrThrow(userId, 'User id');
+    if (isNaN(Number(userId))) {
+      throw new NotAcceptableException(notAcceptableTypeParamMessage('User id', 'Number'));
+    } 
       
     try {
-      const userOrThrow = await this._prismaService.user.findUnique({ where: { id: userId }});
+      const userOrThrow = await this._prismaService.user.findUnique({ where: { id: Number(userId) }});
 
       if(!userOrThrow){
         throw new NotFoundException(notFoundExceptionMessage('User'));
