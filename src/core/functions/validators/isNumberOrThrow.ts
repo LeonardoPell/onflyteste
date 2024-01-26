07a46b40c1193516c340e141aa8cc453f73efc59
LeonardoPell <leonardojbpellegrino@gmail.com>
2024-01-26 09:22:27 -0300

    chore: removendo funcao isNumber

--- src/core/functions/validators/isNumberOrThrow.ts
+++
@@ -1,9 +0,0 @@
-import { HttpException, HttpStatus } from "@nestjs/common";
-
-export default function isNumberOrThrow(numberToValidate: number, item: string){
-    if (isNaN(Number(numberToValidate))) {
-        throw new HttpException(`${item} are not a number`, HttpStatus.NOT_ACCEPTABLE);
-    }
-
-    return Number(numberToValidate); 
-}
