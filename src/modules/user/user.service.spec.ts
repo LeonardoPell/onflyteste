f0f97a0a18129eb079c6fa3f728b49362080ff46
LeonardoPell <leonardojbpellegrino@gmail.com>
2024-01-26 09:22:02 -0300

    test: adicionando teste de user service

---
+++ src/modules/user/user.service.spec.ts
@@ -0,0 +1,55 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+import { PrismaService } from 'src/prisma-module/prisma.service';
+import { User } from '@prisma/client';
+import { HttpException, NotAcceptableException, NotFoundException } from '@nestjs/common';
+
+describe('UserService', () => {
+  let service: UserService;
+  let prisma: PrismaService;
+
+  const mockUser: User = {
+    id: 1,
+    name: 'onflyUser'
+  }
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              findUnique: jest.fn().mockResolvedValue(mockUser)
+            },
+          }
+        },
+      ],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+    expect(prisma).toBeDefined();
+  });
+
+  describe('findOneById', () => {
+    it('should find and return a user', async () => {
+
+      const result = await service.findOneById(1);
+
+      expect(result).toEqual(mockUser);
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw HttpException if no user is found', () => {
+
+      jest.spyOn(prisma.user, 'findUnique').mockRejectedValueOnce(new NotFoundException());
+
+      expect(service.findOneById(2)).rejects.toThrow(HttpException);
+    });
+  });
+});
