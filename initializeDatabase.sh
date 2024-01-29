0b3e031102f4d556ea1f6e9703b5689c4dfcab13
LeonardoPell <leonardojbpellegrino@gmail.com>
2024-01-29 00:11:21 -0300

    chore: finalizando arquivo de inicializacao database

--- initializeDatabase.sh
+++ initializeDatabase.sh
@@ -2,5 +2,3 @@
 
 docker network create --driver bridge postgres-network
 docker run -d --name postgres-container --network=postgres-network -e POSTGRES_DB=onflyTeste -e POSTGRES_USER=onflyTeste -e POSTGRES_PASSWORD=onflyTeste123 -p 5432:5432 postgres
-#docker run --name teste-pgadmin --network=postgres-network -p 15432:80 -e "PGADMIN_DEFAULT_EMAIL=teste@teste.com.br" -e "PGADMIN_DEFAULT_PASSWORD=PgAdmin2024!" -d dpage/pgadmin4
-npx prisma migrate dev --name init
