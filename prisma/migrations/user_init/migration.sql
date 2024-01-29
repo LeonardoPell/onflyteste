5e5f2c0b04480af5533de67f26ff4dd950cad689
LeonardoPell <leonardojbpellegrino@gmail.com>
2024-01-29 00:21:50 -0300

    feat: adiciona migration que cria usuarios

---
+++ prisma/migrations/user_init/migration.sql
@@ -0,0 +1,2 @@
+INSERT INTO public."User" ("name", "email") VALUES ('onflyUser', 'onflytesteleonardo@gmail.com');
+INSERT INTO public."User" ("name", "email") VALUES ('onflyUser2', 'onflytesteleonardo@gmail.com');
